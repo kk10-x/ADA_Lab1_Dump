afc0106dd981c6369952f0aa945e11f4a79b0b5b
kk10-x <53857387+kk10-x@users.noreply.github.com>
2020-03-10 00:52:45 +0530

    Update PrintCommonElementsInDifferentArrays.py

--- PrintCommonElementsInDifferentArrays.py
+++ PrintCommonElementsInDifferentArrays.py
@@ -9,8 +9,8 @@ def check(a,b,c):
                     if(b[y]==c[z]):
                         print("The number ",c[z]," is common in all the above arrays")
 def main():
-    a=input("Enter the elements of the first array").split()
-    b=input("Enter the elements of the second array").split()
-    c=input("Enter the elements of the third array").split()
+    a=input("Enter the elements of the first array ").split()
+    b=input("Enter the elements of the second array ").split()
+    c=input("Enter the elements of the third array ").split()
     check(a,b,c)
 main()
